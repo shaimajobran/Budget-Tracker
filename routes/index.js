0adfbb3faf47711f43d7e08eb93c3e841cf114b0
Shaima Jobran <shaimajobran22@gmail.com>
2021-07-01 16:29:35 -0400

    creating a new route file to handle get requests

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,7 @@
 //creating this route file to handle get requests
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+router.use("/api", apiRoutes);
+
+module.exports = router;
